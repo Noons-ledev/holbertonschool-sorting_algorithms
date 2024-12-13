5b05da7c98c4c05eb5690d85f5e7c31a540d4be4
JESTER-7 <arthur.urbano33@gmail.com>
2024-12-13 01:49:39 +0100

    add the print array function in the code

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,35 +2,78 @@
 /**
  * swap - swap 2 int
  * @a: 1st int
- * @b: 2nd int 
+ * @b: 2nd int
+ * @size: sizeof array
  */ 
-void swap(int *a, int *b)
+void swap(int *array, size_t size, size_t a, size_t b)
 {
-    int *temp;
+	int temp;
 
-    temp = *a;
-    *a = *b;
-    *b = temp;
+	if (a != b)
+	{
+		temp = array[a];
+		array[a] = array[b];
+		array[b] = temp;
+		print_array(array, size);
+	}
 }
 /**
- * partition -
- * @array: pointer to integer array
- * @size: size of the array
+ * partition - split the array for the sort
+ * @array: pointer to int array
+ * @size: sizeof array
+ * @low: lower index of partition
+ * @high: higher index of partition
+ * Return: the pivot
  */
-int partition(int *array, size_t size)
+int partition(int *array, size_t low, size_t high, size_t size)
 {
-    size_t i, j, point;
+	int pivot;
+	size_t i, j;
 
-    point = array[size];
+	if (array == NULL || low >= high)
+		return (low);
+	pivot = array[high]; /* choose last element */
+	i = low - 1; /* smaller element */
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			swap(array, size, i , j);
+		}
+	}
+	swap(array, size, i + 1, high);
+	return (i + 1);
+}
+/**
+ * recursive - recusiv function for quick sort
+ * @array: pointer to int array
+ * @low: lower index of partition
+ * @high: higer index of partition
+ * @size: sizeof array
+ */
+void recursive(int *array, size_t low, size_t high, size_t size)
+{
+	if (low >= high)
+		return;
+	if (low < high)
+	{	
+		size_t p;
 
+		p = partition(array, low, high, size); /* find the index */
+		if (p > 0) /* sort element before and after the partition */
+			recursive(array, low, p - 1, size);
+		recursive(array, p + 1, high, size);
+	}
 }
 /**
  * quick_sort - quick sort algorithm
  * @array: pointer to integer array
- * @size: size of the array
+ * @size: sizeof array
  */
 void quick_sort(int *array, size_t size)
 {
-    size_t i, j;
-    partition (array, size);
+	if (array == NULL || size < 2)
+		return;
+	recursive(array, 0, size - 1, size);
 }
